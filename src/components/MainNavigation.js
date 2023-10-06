2aa87204f88b8d0c16fe2c531217fea801cd1693
Ankur Gupta <ankurnecessary@gmail.com>
2023-10-07 08:28:29 +1030

    Some unrequired files are deleted

--- src/components/MainNavigation.js
+++
@@ -1,8 +0,0 @@
-import logo from '../images/logo.png';
-const MainNavigation = () => {
-    return <header>
-        <h1><img src={logo} alt="" /></h1>
-    </header>;
-}
-
-export default MainNavigation;
