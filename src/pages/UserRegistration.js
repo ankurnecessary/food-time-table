6d32f3c23d1d39dc1b8a88fc1eaf3ec67bc7e2b3
Ankur Gupta <ankurnecessary@gmail.com>
2023-10-08 08:03:44 +1030

    Added user registration <form> HTML

--- src/pages/UserRegistration.js
+++ src/pages/UserRegistration.js
@@ -1,7 +1,34 @@
 const UserRegistration = () => {
     return (<div className="page">
         <h2>User Registration</h2>
-        <hr />
+        <form action="" method="post">
+            <div className="form-group">
+                <label htmlFor="first-name">First name:</label>
+                <input type="text" id="first-name" name="first-name" autoComplete="true" required />
+            </div>
+            <div className="form-group">
+                <label htmlFor="last-name">Last name:</label>
+                <input type="text" id="last-name" name="last-name" autoComplete="true" required />
+            </div>
+            <div className="form-group">
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" autoComplete="true" required />
+            </div>
+            <div className="form-group">
+                <label htmlFor="username">Username:</label>
+                <input type="text" id="username" name="username" autoComplete="true" required />
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" name="password" required />
+            </div>
+            <div className="form-group">
+                <label htmlFor="confirm_password">Confirm Password:</label>
+                <input type="password" id="confirm_password" name="confirm_password" required />
+            </div>
+            <button className="btn form-btn mb-small" type="submit">Register</button>
+            <button className="btn form-btn" type="reset">Reset</button>
+        </form>
     </div>);
 }
 
